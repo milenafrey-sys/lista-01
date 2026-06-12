a45f8e6e969943867975a750440d980019169465
Milena valente <milena_frey@estudante.sesisenai.org.br>
2026-06-11 21:52:16 -0300

    questão 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Informe a idade do visitante: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 16)
+{
+    Console.WriteLine("Entrada liberada.");
+}
+else
+{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}
