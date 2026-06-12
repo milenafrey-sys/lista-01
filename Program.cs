825428cab5076dc63979d6773447d57e840534cb
Milena valente <milena_frey@estudante.sesisenai.org.br>
2026-06-11 21:55:47 -0300

    questão 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿Console.Write("Informe a idade do visitante: ");
-int idade = int.Parse(Console.ReadLine());
+﻿Console.Write("Informe a nota final: ");
+double nota = double.Parse(Console.ReadLine());
 
-if (idade >= 16)
+if (nota >= 7)
 {
-    Console.WriteLine("Entrada liberada.");
+    Console.WriteLine("Estudante aprovado.");
+}
+else if (nota >= 5)
+{
+    Console.WriteLine("Estudante em recuperação.");
 }
 else
 {
-    Console.WriteLine("Entrada permitida somente com responsável.");
+    Console.WriteLine("Estudante reprovado.");
 }
